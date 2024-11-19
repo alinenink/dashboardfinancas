0f85af18f5cde97ca996e07e26b8e60b5eac6a95
aline <alinenink@gmail.com>
2024-11-19 19:50:54 -0300

    config deploy

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
-/** @type {import('next').NextConfig} */
 const isProduction = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  output: 'export', // Exportação estática
-  basePath: isProduction ? '/alineninkfinancas.github.io' : '', // Caminho base para produção
-  assetPrefix: isProduction ? '/alineninkfinancas.github.io' : '', // Prefixo para os recursos
-  trailingSlash: true, // Adiciona barra no final das URLs
+  output: 'export',
+  basePath: isProduction ? '/alineninkfinancas.github.io' : '',
+  assetPrefix: isProduction ? '/alineninkfinancas.github.io' : '',
+  trailingSlash: true,
   images: {
-    unoptimized: true, // Desabilita otimização de imagens para exportação estática
+    unoptimized: true,
   },
 };
