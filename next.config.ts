86866585a4f28926373cf3197e7f864e4ad204d0
aline <alinenink@gmail.com>
2024-11-20 15:50:19 -0300

    geração de docs

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,10 @@
 const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = {
-  output: 'export',
-  distDir: 'out', // Diretório de saída para build
-  basePath: isProduction ? "/alineninkfinancas.github.io/docs" : "/out",
-  assetPrefix: isProduction ? "/alineninkfinancas.github.io/docs" : "/out",
+  output: "export",
+  distDir: "docs", // Diretório de saída para build
+  basePath: "/docs",
+  assetPrefix: "/docs",
   trailingSlash: true, // Adiciona barra ao final das URLs
   images: {
     unoptimized: true, // Necessário para exportação estática
